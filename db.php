514db9c18c951e5bb6c891d3df4906e0659714a3
Song Richard <73071487+Rizinkovic@users.noreply.github.com>
2025-05-30 09:17:13 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "sql211.infinityfree.com"; // Replace with your host
-$username = "if0_39077665";      // Replace with your username
-$password = "salemx1234";      // Replace with your password
-$dbname = "if0_39077665_myblog";   // Replace with your DB name
+$username = "";      // Replace with your username
+$password = "";      // Replace with your password
+$dbname = "";   // Replace with your DB name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
